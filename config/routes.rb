7c6669bdc8874b6dac9e1f667378de30422b8716
lola martinhao <lola.martinhao@gmail.com>
2022-11-16 15:12:55 -0300

    Finished backend codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   resources :restaurants do
+    resources :reviews, only: %i[new create]
     collection do
       get :top
     end
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
       get :chef
     end
   end
+  resources :reviews, only: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
